cc5991bf4ecbf2740ce250d8fc6aee4a6d76478c
steelrat <steelrat@98a6af76-d760-e011-b5d3-001517e85bc2>
2012-12-17 19:56:30 +0000

    a little changes
    git-svn-id: svn://svn.old-games.ru/translate/soft/utt@156 98a6af76-d760-e011-b5d3-001517e85bc2

--- src/gui/simplegui.cpp
+++ src/gui/simplegui.cpp
@@ -91,3 +91,97 @@ BitmapRibbonGui::~BitmapRibbonGui()
 	mScrollRightBtn->Disconnect( wxEVT_LEFT_UP, wxMouseEventHandler( BitmapRibbonGui::OnMouseUp ), NULL, this );
 	
 }
+
+CreateProjectGui::CreateProjectGui( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	
+	this->Centre( wxBOTH );
+}
+
+CreateProjectGui::~CreateProjectGui()
+{
+}
+
+AboutBoxGui::AboutBoxGui( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	wxGridSizer* gSizer14;
+	gSizer14 = new wxGridSizer( 1, 1, 0, 0 );
+	
+	m_panel3 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	wxFlexGridSizer* fgSizer13;
+	fgSizer13 = new wxFlexGridSizer( 2, 1, 0, 0 );
+	fgSizer13->AddGrowableCol( 0 );
+	fgSizer13->AddGrowableRow( 0 );
+	fgSizer13->SetFlexibleDirection( wxBOTH );
+	fgSizer13->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+	
+	wxFlexGridSizer* fgSizer15;
+	fgSizer15 = new wxFlexGridSizer( 0, 2, 0, 0 );
+	fgSizer15->SetFlexibleDirection( wxBOTH );
+	fgSizer15->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+	
+	
+	fgSizer13->Add( fgSizer15, 1, wxEXPAND, 5 );
+	
+	mCloseButton = new wxButton( m_panel3, wxID_OK, wxT("OK"), wxDefaultPosition, wxDefaultSize, 0 );
+	fgSizer13->Add( mCloseButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5 );
+	
+	
+	m_panel3->SetSizer( fgSizer13 );
+	m_panel3->Layout();
+	fgSizer13->Fit( m_panel3 );
+	gSizer14->Add( m_panel3, 1, wxEXPAND, 5 );
+	
+	
+	this->SetSizer( gSizer14 );
+	this->Layout();
+	
+	this->Centre( wxBOTH );
+}
+
+AboutBoxGui::~AboutBoxGui()
+{
+}
+
+SelectModuleGui::SelectModuleGui( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	wxStaticBoxSizer* mBoxSizer;
+	mBoxSizer = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, wxEmptyString ), wxVERTICAL );
+	
+	mModulesChoice = new wxComboBox( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, wxCB_DROPDOWN|wxCB_READONLY ); 
+	mBoxSizer->Add( mModulesChoice, 0, wxALL|wxEXPAND, 5 );
+	
+	wxGridSizer* gSizer6;
+	gSizer6 = new wxGridSizer( 1, 2, 0, 0 );
+	
+	mOkBtn = new wxButton( this, wxID_OK, wxT("OK"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer6->Add( mOkBtn, 0, wxALIGN_CENTER_VERTICAL|wxALIGN_CENTER_HORIZONTAL, 5 );
+	
+	mCancelBtn = new wxButton( this, wxID_CANCEL, wxT("Cancel"), wxDefaultPosition, wxDefaultSize, 0 );
+	gSizer6->Add( mCancelBtn, 0, wxALIGN_CENTER_VERTICAL|wxALIGN_CENTER_HORIZONTAL, 5 );
+	
+	
+	mBoxSizer->Add( gSizer6, 0, wxEXPAND, 5 );
+	
+	
+	this->SetSizer( mBoxSizer );
+	this->Layout();
+	
+	this->Centre( wxBOTH );
+	
+	// Connect Events
+	mOkBtn->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( SelectModuleGui::OnBtnClick ), NULL, this );
+}
+
+SelectModuleGui::~SelectModuleGui()
+{
+	// Disconnect Events
+	mOkBtn->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( SelectModuleGui::OnBtnClick ), NULL, this );
+	
+}
--- src/gui/simplegui.h
+++ src/gui/simplegui.h
@@ -22,12 +22,17 @@
 #include <wx/button.h>
 #include <wx/sizer.h>
 #include <wx/panel.h>
+#include <wx/dialog.h>
+#include <wx/combobox.h>
+#include <wx/statbox.h>
 
 ///////////////////////////////////////////////////////////////////////////
 
 #define wxID_BITMAP_RIBBON 2000
 #define wxID_SCROLLLEFT_BTN 2001
 #define wxID_SCROLLRIGHT_BTN 2002
+#define wxID_CREATE_PROJECT_DLG 2003
+#define wxID_SELECT_MODULE_ID 2004
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Class BitmapRibbonGui
@@ -52,6 +57,63 @@ class BitmapRibbonGui : public wxPanel
 		
 		BitmapRibbonGui( wxWindow* parent, wxWindowID id = wxID_BITMAP_RIBBON, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,100 ), long style = wxTAB_TRAVERSAL ); 
 		~BitmapRibbonGui();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class CreateProjectGui
+///////////////////////////////////////////////////////////////////////////////
+class CreateProjectGui : public wxDialog 
+{
+	private:
+	
+	protected:
+	
+	public:
+		
+		CreateProjectGui( wxWindow* parent, wxWindowID id = wxID_CREATE_PROJECT_DLG, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 478,393 ), long style = wxDEFAULT_DIALOG_STYLE ); 
+		~CreateProjectGui();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class AboutBoxGui
+///////////////////////////////////////////////////////////////////////////////
+class AboutBoxGui : public wxDialog 
+{
+	private:
+	
+	protected:
+		wxPanel* m_panel3;
+		wxButton* mCloseButton;
+	
+	public:
+		
+		AboutBoxGui( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 505,371 ), long style = wxDIALOG_NO_PARENT ); 
+		~AboutBoxGui();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class SelectModuleGui
+///////////////////////////////////////////////////////////////////////////////
+class SelectModuleGui : public wxDialog 
+{
+	private:
+	
+	protected:
+		wxComboBox* mModulesChoice;
+		wxButton* mOkBtn;
+		wxButton* mCancelBtn;
+		
+		// Virtual event handlers, overide them in your derived class
+		virtual void OnBtnClick( wxCommandEvent& event ) { event.Skip(); }
+		
+	
+	public:
+		
+		SelectModuleGui( wxWindow* parent, wxWindowID id = wxID_SELECT_MODULE_ID, const wxString& title = wxT("Select Lua module:"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 307,106 ), long style = wxCAPTION|wxCLOSE_BOX|wxSYSTEM_MENU ); 
+		~SelectModuleGui();
 	
 };
 
